71f07cd77f6537668fd91828c18273353caeeb20
muhammad ikram <75674214+ikram-tech@users.noreply.github.com>
2024-04-12 11:01:06 +0500

    Add files via upload

---
+++ New folder (9)/app.js
@@ -0,0 +1,9 @@
+class App{
+    constructor(){
+        this.$form=document.querySelector('#form');
+        this.$noteTitle=document.querySelector('#note-title');
+        this.$formButtons=document.querySelector('#form-buttons');
+        this.addEventListeners();
+
+    }
+}
